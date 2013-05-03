ea758000f8ea4882e3c59e7fdb60a5d69cd64a3d
mfkaptan <mfkaptan@outlook.com>
2013-05-03 21:19:07 +0300

    slideshow update

--- slideshow.py
+++ slideshow.py
@@ -1,34 +1,10 @@
 # # Mustafa Kaptan
 # # Slideshow program
+
 import sys, ConfigParser, time
-from SimpleCV import *
-import pygame
-
-# Colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-BACKGROUND = (100, 100, 100)
-
-def start_countdown(screen):
-    # Get screens width,height -> center
-    centerX = screen.get_width() / 2
-    centerY = screen.get_height() / 2
-
-    # Font
-    countfont = pygame.font.Font('turkish.ttf', 150)
-
-    # Start screen
-    startScreen = Image()
-    for i in range (4, 0, -1):
-
-        if i > 1:
-            # Draw an anti-aliased circle
-            #gfxdraw.aacircle(screen, centerX, centerY, 150, WHITE)
-            newlayer.circle((centerX,centerY), 150, filled=False, color = Color.WHITE)
-            counttext = countfont.render("" + str(i - 1), True, WHITE)
-            screen.blit(counttext, (centerX - 40, centerY - 90))
-        pygame.display.flip()
-        pygame.time.delay(1000)
+from SimpleCV import Image
+from SimpleCV import ImageSet
+from SimpleCV import Display
 
 def main():
     # Get file name
@@ -45,16 +21,8 @@ def main():
         print "Error: can\'t find file or read data"
         sys.exit()
 
-    pygame.init()
-    # Drawing layer
-    #newlayer = DrawingLayer((width,height))
-
-    # Screen
-    display = Display(flags=pygame.FULLSCREEN)
-    screen.set_caption("Slideshow")
-
-    # Invisible mouse
-    pygame.mouse.set_visible(False)
+    # Display
+    display = Display()
 
     # Set of slide images
     slideSet = ImageSet("./experiments/" + filename)
@@ -63,12 +31,9 @@ def main():
     length = len(lineList)
     sleepList = list()
 
-    # Save lines to Image class and Slideshow list
+    # Sleep times
     for line in lineList:
         sleepList.append(int(line[1])/1000)
-
-    # Start the countdown
-    start_countdown()
 
     # Main loop    
     while display.isNotDone():
@@ -80,3 +45,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
